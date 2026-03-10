7b4be314ae64b15947b4470fa95290c3b736c508
parvesh95 <growwyouteam@gmail.com>
2026-03-11 00:14:35 +0530

    first commit1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 // Change this to your PC's local IP when testing on a physical device
-const API_BASE = 'http://10.195.110.228:5000/api';
-const WS_BASE = 'http://10.195.110.228:5000';
+// const API_BASE = 'http://10.195.110.228:5000/api';
+// const WS_BASE = 'http://10.195.110.228:5000'
+const API_BASE = 'https://scrapx-br.vercel.app/api';
+const WS_BASE = 'https://scrapx-br.vercel.app';
 
 export { API_BASE, WS_BASE };
